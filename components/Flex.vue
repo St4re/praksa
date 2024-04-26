e7bc404cc6e6b1a80a54cc0ee13abc93754cbed6
AresOsredkar <aresosredkar1337@gmail.com>
2024-04-26 14:52:25 +0200

    26.4.24 14:51 Petek

---
+++ components/Flex.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        flexDirection: {
+            type: String,
+            default: "row",
+        },
+        flexWrap: String,
+        flexBasis: String,
+        justifyContent: String,
+        alignItems: String,
+        flexGap: String,
+        flexGrow: String,
+        flexShrink: String,
+    },
+    setup(props) {
+
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+    display:flex;
+}
+</style>
